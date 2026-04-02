bdd0a9062a7877dff460bb1845cdecb1b37c7060
Naufall-Cade <naufal1syafri23@gmail.com>
2026-04-02 12:51:38 +0700

    Update Data Latihan R
    -

---
+++ Learn R (Awal)/Belajar R bener.R
@@ -0,0 +1,34 @@
+
+
+# Mendefinisikan 
+
+a <- "Aku"
+b = 9
+c <- 2
+
+d <- b + c
+b + a
+e <- "Kita"
+
+paste0(a,e)
+
+f <- paste0(a, "-", e)
+
+#install package
+install.packages("psych")
+- Untuk install packages yang tersedia di R
+library("psych")
+- Untuk melihat apakah pakcages yang di install sudah aktif atau belum.
+
+===================================================================
+
+# Manajemen Data
+#df <- read.csv("Twisk.csv")
+#Df = Nama/sebutan file
+Read.csv untuk melakukan data input beri tanda kutip 
+untuk memanggil spesifik nama file
+- Pindahkan file ke directory file R disimpan.
+
+#View(df)#
+Untuk melihat file yang sudah di input
+
